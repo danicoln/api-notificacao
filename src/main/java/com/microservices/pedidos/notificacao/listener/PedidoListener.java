c9bf93d4aef448bf4aba86aaba0addb5b4617d69
Daniel Lincoln <lincolndeveloper@outlook.com>
2025-01-31 18:32:27 -0300

    Refs. #23 - Desenvolvido o Listener para receber pedidos da fila

---
+++ src/main/java/com/microservices/pedidos/notificacao/listener/PedidoListener.java
@@ -0,0 +1,18 @@
+package com.microservices.pedidos.notificacao.listener;
+
+import com.microservices.pedidos.notificacao.entity.Pedido;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PedidoListener {
+
+    private final Logger logger = LoggerFactory.getLogger(PedidoListener.class);
+
+    @RabbitListener(queues = "pedidos.v1.pedido-criado.gerar-notificacao")
+    public void enviarNotificacao(Pedido pedido) {
+        logger.info("Notificação gerada: {}", pedido);
+    }
+}
