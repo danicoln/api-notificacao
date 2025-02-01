9149b5a72736c75a4f0e4b406e1d54a9b052cd02
Daniel Lincoln <lincolndeveloper@outlook.com>
2025-01-31 23:29:16 -0300

    Refs. #25 - Adicionando o service no PedidoListener e ajustes do lombok no pom.xml

--- src/main/java/com/microservices/pedidos/notificacao/listener/PedidoListener.java
+++ src/main/java/com/microservices/pedidos/notificacao/listener/PedidoListener.java
@@ -1,18 +1,24 @@
 package com.microservices.pedidos.notificacao.listener;
 
 import com.microservices.pedidos.notificacao.entity.Pedido;
+import com.microservices.pedidos.notificacao.service.EmailService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class PedidoListener {
 
     private final Logger logger = LoggerFactory.getLogger(PedidoListener.class);
 
+    private final EmailService emailService;
+
     @RabbitListener(queues = "pedidos.v1.pedido-criado.gerar-notificacao")
     public void enviarNotificacao(Pedido pedido) {
+        emailService.enviarEmail(pedido);
         logger.info("Notificação gerada: {}", pedido);
     }
 }
